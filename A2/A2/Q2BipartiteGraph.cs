fc7bed36951e391ab18fe799d526f6892ef4fbd3
zahrahosseini99 <hosseini99.zahra@gmail.com>
2020-02-22 18:28:12 +0330

    A2

---
+++ A2/A2/Q2BipartiteGraph.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using TestCommon;
+
+namespace A2
+{
+    public class Q2BipartiteGraph : Processor
+    {
+        public Q2BipartiteGraph(string testDataName) : base(testDataName) { }
+
+        public override string Process(string inStr) =>
+            TestTools.Process(inStr, (Func<long, long[][], long>)Solve);
+
+        public long Solve(long NodeCount, long[][] edges)
+        {
+            UnDirectedGraph graph = new UnDirectedGraph(edges, NodeCount);
+            return graph.isBipartite();
+        }
+    }
+}
