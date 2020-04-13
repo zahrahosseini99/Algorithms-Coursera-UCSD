38d5430dd3e9f6cd4587273215cb3c6f6692ed1f
zahrahosseini99 <hosseini99.zahra@gmail.com>
2020-04-13 13:28:50 +0430

    Q2

---
+++ Q2/Q2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Q2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        public static void Table(string pattern)
+        {
+            Dictionary<char, int> res = new Dictionary<char, int>();
+            for (int i = 0; i < pattern.Length; i++)
+            {
+                res.Add(pattern[i], pattern.Length - i - 1);
+            }
+
+        }
+    }
+}
