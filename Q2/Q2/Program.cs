49453cc3089c93f1e748d8de7f043fda65de265b
zahrahosseini99 <hosseini99.zahra@gmail.com>
2020-04-13 16:55:16 +0430

    Q2

--- Q2/Q2/Program.cs
+++ Q2/Q2/Program.cs
@@ -7,20 +7,45 @@ namespace Q2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string txt = "WELCOMETOTEAMMAST";
+            string pat = "TEAMMAST";
+            Dictionary<char, int> table = new Dictionary<char, int>();
+            table = Table(pat);
+            Console.WriteLine(boyerMoore(txt, pat, table));
         }
-        public static void Table(string pattern)
+        public static Dictionary<char, int> Table(string pattern)
         {
-            List<int> res = new List<int>();
+
             Dictionary<char, int> table = new Dictionary<char, int>();
             for (int i = 0; i < pattern.Length; i++)
             {
-                table.Add(pattern[i], pattern.Length - i - 1);
+                table[pattern[i]] = Math.Max(1, pattern.Length - i - 1);
             }
-
+            table['*'] = pattern.Length;
+            return table;
         }
-        public void boyerMoore(string text, Dictionary<char, int> table)
+        public static int boyerMoore(string text, string pattern, Dictionary<char, int> table)
         {
+            int shift = 0;
+            int res = 0;
+            while (shift <= (text.Length - pattern.Length))
+            {
+                int j = pattern.Length - 1;
+                while (j >= 0 && pattern[j] == text[shift + j])
+                {
+                    j--;
+                }
+                if (j < 0)
+                {
+                   res=shift;
+                    break;
+                }
+                else
+                {
+                    shift += Math.Max(1, j - table[text[shift + j - 1]]);
+                }
+            }
+            return res;
 
         }
     }
