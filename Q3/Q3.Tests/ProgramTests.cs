5ee52e2864c75143d1d02300207adce21f6e415d
zahrahosseini99 <hosseini99.zahra@gmail.com>
2020-04-21 21:10:57 +0430

    Q3

---
+++ Q3/Q3.Tests/ProgramTests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ta_class;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace program.Tests
+{
+    [TestClass()]
+    public class ProgramTests
+    {
+        [TestMethod()]
+        public void SearchTest()
+        {
+            string text = "aabxaabxcaabxaabxay";
+            string patern = "aabx";
+            int[] resualts = { 0, 4, 9,13};
+
+            List<int> res = Program.search(text, patern).ToList();
+            CollectionAssert.AreEqual(resualts, res);
+
+  
+        }
+    }
+}
